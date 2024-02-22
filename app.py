88fa591ed954858dfc87327312f1338a2ad150b2
santiagoruizm01 <160878816+santiagoruizm01@users.noreply.github.com>
2024-02-22 10:11:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ import streamlit as st
 from PIL import Image
 
 st.title(" Aplicación INTERFACES ")
+
+st.header(" En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write(" Fácilmente puedo realizar backend y frontend")
+image = Image.open('Interfaces Multi2.png')
+
+st.image(image, caption'Interfaces multimodales')
